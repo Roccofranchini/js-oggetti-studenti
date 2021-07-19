fd80dbc72c875e1c89c4cd0140bed645f588d5f0
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-19 15:34:53 +0200

    fx

---
+++ js/script1.js
@@ -0,0 +1,32 @@
+/* 
+------------------ES 1------------------
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+1- creare un oggetto con le rispettive proprietò
+2- utilizzare un ciclo for-in per stamparle in pagina
+*/
+
+
+// ------------------ESERCIZIO 1------------------
+
+
+var display = document.getElementById('display');
+var student = {
+    name: 'Giuseppe',
+    surname: 'Conte',
+    age: 22,
+}
+
+var studentsProps = '';
+
+for (var key in student) {
+    var studentProp = (student[key]);
+    studentsProps += '<li>' + studentProp + '</li>';
+}
+
+display.innerHTML = studentsProps;
+
+
+
+// ------------------ESERCIZIO 1------------------
---
+++ js/script2.js
@@ -0,0 +1,22 @@
+/*
+------------------ES 2------------------
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+Consigli del giorno:
+Iniziate col riflettere sugli step che dovete eseguire, scrivendoli in italiano sotto forma di commenti
+Facciamo un passo alla volta e verifichiamo i nostri passi col console.log.
+Quando tutto funziona col console.log possiamo preoccuparci di eseguire in pagina!
+
+1- Creare un array di oggetti di studenti con proprietà nome, cognome e eta
+2- 
+
+
+*/
+
+var students [
+    {
+        name: 'leonardo',
+        
+    }
+]
